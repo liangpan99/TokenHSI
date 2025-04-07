1fdb98540bdaaf28d4e04ed8d0bff8202116c2c9
LeyangWen <wenleyan@umich.edu>
2025-04-07 10:01:57 -0400

    update readme

--- run_gui.sh
+++ run_gui.sh
@@ -1,7 +1,7 @@
 # conda activate tokenhsi # need python 3.8, so you cant load python3.10-anaconda etc, or used the module load pytorch
 
-#export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
-#export LD_LIBRARY_PATH="/home/wenleyan/projects/isaacgym/python/isaacgym/_bindings/linux-x86_64:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="/home/wenleyan/projects/isaacgym/python/isaacgym/_bindings/linux-x86_64:$LD_LIBRARY_PATH"
 
 CUDA_LAUNCH_BLOCKING=1 python ./tokenhsi/run.py --task HumanoidCarry \
     --cfg_train tokenhsi/data/cfg/train/rlg/amp_imitation_task.yaml \
@@ -11,6 +11,6 @@ CUDA_LAUNCH_BLOCKING=1 python ./tokenhsi/run.py --task HumanoidCarry \
     --output_path /scratch/shdpm_root/shdpm0/wenleyan/tokenhsi/carry1/ \
     --test \
     --record_headless \
-    --num_envs 1 #> out.txt
+    --num_envs 1 > gui_out.txt
 
 # sh tokenhsi/scripts/single_task/traj_test.sh
